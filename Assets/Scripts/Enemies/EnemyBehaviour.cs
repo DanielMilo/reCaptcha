cc73bd7064110f2181ae05d3345275e091dc6ef5
Matan Maron <matanmaron@gmail.com>
2019-07-14 15:48:57 +0300

    begin enemy behaviour

---
+++ Assets/Scripts/Enemies/EnemyBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private float LoudRange;
+    [SerializeField]
+    private float SilentRange;
+    [SerializeField]
+    private float SightRange;
+
+    private GameObject Player;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool doesNoticePlayer = false;
+
+        // Check 
+
+
+
+        if (doesNoticePlayer)
+        {
+            // Attack
+        }
+        else
+        {
+            // Idle
+        }
+    }
+}
