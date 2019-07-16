226c888eceafd6cb0ccf64e420e56b791cf4f5ae
Alon Wolf <alonw125@gmail.com>
2019-07-16 12:28:33 +0300

    kill enemies

--- Assets/Scripts/Combat/ExplosionBehaviour.cs
+++ Assets/Scripts/Combat/ExplosionBehaviour.cs
@@ -4,21 +4,26 @@ using UnityEngine;
 
 public class ExplosionBehaviour : MonoBehaviour
 {
-    [SerializeField] float timeToLive;
-    private float creationTime;
+    [SerializeField] int damage = 1;
 
     // Start is called before the first frame update
-    void Awake()
+    private void Start()
     {
-        creationTime = Time.time;
+        var colliders = Physics2D.OverlapCircleAll(this.transform.position, this.transform.localScale.x);
+        foreach(var collider in colliders)
+        {
+            var health = collider.GetComponent<Health>();
+            if (health)
+            {
+                health.TakeDamage(damage);
+            }
+        }
+
+        Destroy(gameObject, this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(creationTime + timeToLive <= Time.time)
-        {
-            Destroy(gameObject);
-        }
     }
 }
